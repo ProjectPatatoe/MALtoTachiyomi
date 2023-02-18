7dfad54bf1978d263bead6287980e4269c361a99
ProjectPatatoe <projectpaatt@gmail.com>
2023-02-18 12:56:21 -0800

    update for jikan v4

--- MALtoTachiyomi.ps1
+++ MALtoTachiyomi.ps1
@@ -1,13 +1,12 @@
-#https://api.jikan.moe/v3/manga/28123
-
 #CONFIG
 $CFG_preferEnglishTitles = $true
 $CFG_getCoverImage = $true
 #you can get a test API url from https://jikan.docs.apiary.io/ and trying a request via 'Development Proxy'
-#$CFG_APIurl = "https://private-anon-xxxxxxxx-jikan.apiary-proxy.com/v3"
-$CFG_APIurl = "https://api.jikan.moe/v3"
+#$CFG_APIurl = "https://private-anon-xxxxxxxx-jikan.apiary-proxy.com/v4"
+$CFG_APIurl = "https://api.jikan.moe/v4"
 $DebugPreference = "Continue" #comment to hide
 
+#test manga
 #hinamatsuri 36413 - 1 author story+art
 #log horizon 46024 - has english title, 1author story+art
 #maou 28123 - 1 author 1 artist, null english title
@@ -18,7 +17,10 @@ $DebugPreference = "Continue" #comment to hide
 $mangaID = Read-Host -Prompt "Enter MyAnimeList ID of Manga:"
 $response = Invoke-WebRequest -Uri "$CFG_APIurl/manga/$mangaID"
 #TODO:test if fail
-$data = $response | ConvertFrom-Json
+Write-Debug $response
+$data = $response.Content | ConvertFrom-Json
+$data | ConvertTo-Json | Out-File "data.json"
+$data = $data.data
 $output = New-Object PSObject
 #####title
 Write-Debug "Title"
@@ -32,69 +34,20 @@ else
     Write-Debug "title_english is null"
     Add-Member -InputObject $output -MemberType NoteProperty -Name title -Value $data.title
 }
+Write-Debug $output.title
 #####author/artist
 
 Write-Debug "Author/Artist"
-$author = @()
-$artist = @()
-$personDelay = 1
-if ($data.authors.Length -ge 3)
-    {
-        Write-Debug "At least 3 people, delay now 4 seconds"
-        $personDelay = 4
-        Start-Sleep -Seconds $personDelay #delay once because we just got the initial info
-    }
-foreach ($person in $data.authors)
-{
-    Write-Host ("Person: {0}" -f $person.name)
-    Start-Sleep -Seconds $personDelay #prevent making the jikan api angry
-    $personID = $person.mal_id
-    $personResponseRaw = Invoke-WebRequest -Uri "$CFG_APIurl/person/$personID"
-    #TODO:test if fail
-    $personResponse = ConvertFrom-Json $personResponseRaw.content
-    foreach ($personManga in $personResponse.published_manga)
-    {
-        Write-Debug ("Trying to match: {0}" -f $personManga.manga.mal_id)
-        if ($personManga.manga.mal_id -eq $mangaID)
-        {
-            Write-Debug "Person-Manga Match"
-            if ($personManga.position -eq "Story & Art")
-            {
-                $author += $personResponse.name
-                $artist += $personResponse.name
-                break
-            }
-            elseif ($personManga.position -eq "Story")
-            {
-                $author += $personResponse.name
-                break
-            }
-            elseif ($personManga.position -eq "Art")
-            {
-                $artist += $personResponse.name
-                break
-            }
-            else
-            {
-                Write-Debug ("unknown position: {0}" -f $personManga.position)
-            }
-        }#if match
-    }#foreach person-manga
-}#for eachperson
 #NOTE: Tachiyomi doesn't handle actual multi-author/artists, must mix into 1 string, NO ARRAY!
 #      Gotta smush it into 1 string ): hopefully fixed in Tachoyomi in the future
-$authorStr = $author[0]
-$artistStr = $artist[0]
-for ($idx = 1; $idx -lt $author.Length; ++$idx)
+#NOTE: Jikan v4 doesn't list author vs artist! dump everyone into author ):
+$authorStr = $data.authors[0].name
+for ($idx = 1; $idx -lt $data.authors.Length; ++$idx)
 {
-    $authorStr += (", " + $author[$idx])
-}
-for ($idx = 1; $idx -lt $artist.Length; ++$idx)
-{
-    $artistStr += (", " + $artist[$idx])
+    $authorStr += ("; " + $data.authors[$idx].name)
 }
 Add-Member -InputObject $output -MemberType NoteProperty -Name author -Value $authorStr
-Add-Member -InputObject $output -MemberType NoteProperty -Name artist -Value $artistStr
+
 #####description
 Write-Debug "Description"
 Add-Member -InputObject $output -MemberType NoteProperty -Name description -Value $data.synopsis
@@ -125,10 +78,12 @@ Add-Member -InputObject $output -MemberType NoteProperty -Name '_status values' 
 #####save
 Write-Debug "Save"
 "Saving details..."
+#TODO If it didn't work, dont make a details.json!
+$output
 ConvertTo-Json $output | Out-File -Encoding utf8 "details.json"
 if ($CFG_getCoverImage)
 {
     "Saving cover..."
-    Invoke-WebRequest -Uri $data.image_url -OutFile "cover.jpg"
+    Invoke-WebRequest -Uri $data.images.jpg.image_url -OutFile "cover.jpg"
     #TODO:test if fail
 }
